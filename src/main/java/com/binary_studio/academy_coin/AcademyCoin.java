873dfc7caef86626c4e63db4be21f8fde2a538c0
Dan Smahliuk <55285854+danexpc@users.noreply.github.com>
2021-05-07 13:10:59 +0300

    Implementation of AcademyCoin.java

--- src/main/java/com/binary_studio/academy_coin/AcademyCoin.java
+++ src/main/java/com/binary_studio/academy_coin/AcademyCoin.java
@@ -1,5 +1,6 @@
 package com.binary_studio.academy_coin;
 
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public final class AcademyCoin {
@@ -8,8 +9,23 @@ public final class AcademyCoin {
 	}
 
 	public static int maxProfit(Stream<Integer> prices) {
-		// TODO: Implement
-		return 0;
+
+		int profit = 0;
+
+		Integer[] pricesArray = prices.collect(Collectors.toList()).toArray(Integer[]::new);
+
+		int j = 0;
+		for(int i = 1; i < pricesArray.length; i++){
+			if (pricesArray[i - 1] > pricesArray[i]) {
+				j = i;
+			}
+			if (pricesArray[i - 1] <= pricesArray[i] &&
+					(i + 1 == pricesArray.length || pricesArray[i] > pricesArray[i+1])) {
+				profit += pricesArray[i] - pricesArray[j];
+			}
+		}
+
+		return profit;
 	}
 
 }
