f805589ce7d68a7cc414c02d5239170d96b00ef8
Dan Smahliuk <55285854+danexpc@users.noreply.github.com>
2021-05-07 13:45:39 +0300

    Atack subsystem implementation

--- src/main/java/com/binary_studio/fleet_commander/core/subsystems/AttackSubsystemImpl.java
+++ src/main/java/com/binary_studio/fleet_commander/core/subsystems/AttackSubsystemImpl.java
@@ -4,37 +4,63 @@ import com.binary_studio.fleet_commander.core.common.Attackable;
 import com.binary_studio.fleet_commander.core.common.PositiveInteger;
 import com.binary_studio.fleet_commander.core.subsystems.contract.AttackSubsystem;
 
+import static java.lang.Math.min;
+
 public final class AttackSubsystemImpl implements AttackSubsystem {
 
-	public static AttackSubsystemImpl construct(String name, PositiveInteger powergridRequirments,
+	private final String name;
+	private final PositiveInteger powergridRequirements;
+	private final PositiveInteger capacitorConsumption;
+	private final PositiveInteger optimalSpeed;
+	private final PositiveInteger optimalSize;
+	private final PositiveInteger baseDamage;
+
+	private AttackSubsystemImpl(String name, PositiveInteger powergridRequirements, PositiveInteger capacitorConsumption,
+								PositiveInteger optimalSpeed, PositiveInteger optimalSize, PositiveInteger baseDamage) {
+		this.name = name;
+		this.powergridRequirements = powergridRequirements;
+		this.capacitorConsumption = capacitorConsumption;
+		this.optimalSpeed = optimalSpeed;
+		this.optimalSize = optimalSize;
+		this.baseDamage = baseDamage;
+	}
+
+	public static AttackSubsystemImpl construct(String name, PositiveInteger powergridRequirements,
 			PositiveInteger capacitorConsumption, PositiveInteger optimalSpeed, PositiveInteger optimalSize,
 			PositiveInteger baseDamage) throws IllegalArgumentException {
-		// TODO: Ваш код здесь :)
-		return null;
+		if (name == null || "".equals(name.strip())) {
+			throw new IllegalArgumentException("Name should be not null and not empty");
+		}
+
+		return new AttackSubsystemImpl(name, powergridRequirements, capacitorConsumption, optimalSpeed,
+				optimalSize, baseDamage);
 	}
 
 	@Override
 	public PositiveInteger getPowerGridConsumption() {
-		// TODO: Ваш код здесь :)
-		return null;
+		return this.powergridRequirements;
 	}
 
 	@Override
 	public PositiveInteger getCapacitorConsumption() {
-		// TODO: Ваш код здесь :)
-		return null;
+		return this.capacitorConsumption;
 	}
 
 	@Override
 	public PositiveInteger attack(Attackable target) {
-		// TODO: Ваш код здесь :)
-		return null;
+		double sizeReductionModifier = target.getSize().value() >= optimalSize.value() ? 1
+				: (double)target.getSize().value() / this.optimalSize.value();
+
+		double speedReductionModifier = target.getCurrentSpeed().value() <= optimalSpeed.value() ? 1
+				: (double) optimalSpeed.value() / (2 * target.getCurrentSpeed().value());
+
+		return PositiveInteger.of((int) Math.ceil(baseDamage.value() *
+				min(sizeReductionModifier, speedReductionModifier)));
 	}
 
 	@Override
 	public String getName() {
-		// TODO: Ваш код здесь :)
-		return null;
+		return this.name;
 	}
 
 }
